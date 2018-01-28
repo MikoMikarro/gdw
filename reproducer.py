138c451e732f7971768b259504596d2c876d71e1
MikoMikarro <lopezcuestam@gmail.com>
2018-01-28 22:32:33 +0100

    UnicodeDecodeError

--- reproducer.py
+++ reproducer.py
@@ -5,12 +5,14 @@ from playsound import playsound
 file = open('audio_list.txt','r')
 data = file.readlines()
 file.close()
-paste = "C:/Users/lopez/OneDrive/Escritorio/Codigos/Gestordewebs/"
 from string import split
 while True:
 	for i in data[0:-1]:
-		# try:
-		print('Reproduciendo: "'+str(split(split(i,"/")[0],".")[0])+'" de '+str(split(i,"/")[2]))
-		playsound("songs/"+str(split(i,"/")[3][0:-1])+"/"+str(split(i,"/")[2])+"/"+str(split(i,"/")[1])+"/"+str(split(i,"/")[0]))
-		# except:
-		print('Error no arquivo: "'+str(split(split(i,"/")[0],".")[0])+'" de '+str(split(i,"/")[2]))
+		try:
+			print('Reproduciendo: "'+str(split(split(i,"/")[0],".")[0])+'" de '+str(split(i,"/")[2]))
+			playsound("songs/"+str(split(i,"/")[3][0:-1])+"/"+str(split(i,"/")[2])+"/"+str(split(i,"/")[1])+"/"+str(split(i,"/")[0]))
+		except UnicodeDecodeError:
+			print('Error ascii no arquivo: "'+str(split(split(i,"/")[0],".")[0])+'" de '+str(split(i,"/")[2]))
+		except KeyboardInterrupt:
+			print("saindo...")
+			quit()
