a9933a2fe8ecb179c486a27510a72f605d4e228f
MikoMikarro <lopezcuestam@gmail.com>
2018-01-28 22:28:10 +0100

    Adding reproducing by genre and solving bugs

--- r_x.py
+++ r_x.py
@@ -6,27 +6,27 @@ from sys import executable
 from subprocess import Popen, CREATE_NEW_CONSOLE
 
 def ex():
-    file = open("albumes.txt","r")
+    file = open("genres.txt","r")
     data = file.readlines()
     file.close()
     while True:
         num = 0
-        print("Albumes disponhibles")
+        print("Xeneros disponhibles")
         for i in data[1:]:
             print num," ",i
             num+=1
         ans = input()
         if ans <= num:
-            alb_name == data[ans]
+            xen_name == data[ans]
             break
         else:
-            print "Album non anhadido"
+            print "Xenero non anhadido"
     file = open("audio_data.txt","r")
     data = file.readlines()
     file.close()
     sng_list = []
-    for i in data[ç:-1]:
-        if split(i,"/")[1] == alb_name:
+    for i in data[:-1]:
+        if split(i,"/")[3] == alb_name:
             sng_list.append(i)
     shuffle(sng_list)
     text = ""
@@ -36,4 +36,4 @@ def ex():
     file.write(text)
     file.close()
     Popen('python reproducer.py', creationflags=CREATE_NEW_CONSOLE)
-    return ("Reproducir album - "+alb_name)
+    return ("Reproducir xenero - "+xen_name)
